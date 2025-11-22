08969f62d62ffaf5877d2ee65af465cf94255085
krystlc <alex.valle@gmail.com>
2025-11-22 15:52:05 -0500

    fix: level 3 freeze

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -198,7 +198,6 @@ export default function Index() {
       gameState.isGameOver,
       gameStarted,
       isLevelingUp,
-      isLoopRunning,
       wantsToHardDrop,
       wantsToRotate,
       moveDirection,
