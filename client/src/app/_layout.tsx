58f0e418a948e6d6cea11aba261c665657547453
krystlc <alex.valle@gmail.com>
2026-08-14 18:15:30 -0400

    chore: sentry to client

--- client/src/app/_layout.tsx
+++ client/src/app/_layout.tsx
@@ -1,7 +1,23 @@
 import { Stack } from "expo-router";
 import { GameServerProvider } from "../contexts/GameServerContext";
 
-export default function RootLayout() {
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://486a9141514262e9129e8017893040f1@o4511911256915968.ingest.us.sentry.io/4511911273758720',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+function RootLayout() {
   return (
     <GameServerProvider>
       <Stack
@@ -14,3 +30,5 @@ export default function RootLayout() {
     </GameServerProvider>
   );
 }
+
+export default Sentry.wrap(RootLayout)
